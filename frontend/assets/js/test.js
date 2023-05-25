351b0653e29d3a5c45e7a42eddedefcf890c04a9
Anthony <denayeranthony1782@gmail.com>
2023-05-25 13:52:44 +0200

    input ok

--- frontend/assets/js/test.js
+++ frontend/assets/js/test.js
@@ -1,10 +1,11 @@
 export function test() {
   let eventContainer = document.querySelector(".event-container");
   let inputEvent = document.querySelector(".event-input");
-  let inputEventValue = inputEvent.value;
+
   let eventButton = document.querySelector(".event-button");
 
   eventButton.addEventListener("click", () => {
+    let inputEventValue = inputEvent.value;
     let newDiv = document.createElement("div");
     let content = `${inputEventValue}`;
     newDiv.innerHTML += content;
