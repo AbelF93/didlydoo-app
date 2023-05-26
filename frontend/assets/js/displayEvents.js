8d41b2d1049a92f41a2f207fe021c947c7fa82f2
Abel Fortunati <abel.fortunati@gmail.com>
2023-05-26 14:24:04 +0200

    data search

--- frontend/assets/js/displayEvents.js
+++ frontend/assets/js/displayEvents.js
@@ -7,28 +7,45 @@ export function displayEventsInfo() {
           .then((response) => response.json())
           .then((json) => {
             //getting all event objects
-           /* for (let i = 0; i < json.length; i++) {
-              console.log(json[i].name);
-              console.log(json[i].author);
-              console.log(json[i].description);*/
-
+              for (let i = 0; i < json.length; i++) {
+                  let a = json[i];
+                  console.log(a.name);
+                  console.log(a.author);
+                  console.log(a.description);
+                  for (let j = 0 ; j < a.dates.length; j++){
+                    let day = a.dates[j];
+                    console.log(day.date); 
+                    for (let atd = 0 ; atd < day.attendees.length ; atd ++){
+                        let eventAttendees = day.attendees[atd];
+                        console.log("attendees: " + eventAttendees.name);
+                        console.log("available: " + eventAttendees.available);
+                    }  
+                  }
+                  ;}
+                  
+                
               for(let i = 0;i< json.length;i++){
       
                 let eventContainer = document.createElement("div");
                 let eventName = document.createElement('h2');
-                eventName.textContent = json[i].name;
+                eventName.textContent = json[i].author + "'s " + json[i].name;
                 eventName.setAttribute('class','event-name');
-                let eventAuthor = document.createElement('p');
-                eventAuthor.setAttribute('class','event-author');
-                eventAuthor.textContent = json[i].author;
                 let eventDescription = document.createElement('p');
                 eventDescription.textContent = json[i].description;
                 eventDescription.setAttribute('class','event-description');
                 
                 eventContainer.appendChild(eventName);
-                eventContainer.appendChild(eventAuthor);
                 eventContainer.appendChild(eventDescription);
                 eventList.appendChild(eventContainer);
+                
+                //create a table with the elements
+                const eventTable =document.createElement('table');
+                const eventCoreTable = document.createElement('tbody');
+                //generate cells
+                for (let r = 0; r < 3; r++ ){
+                    const row= document.createElement('tr');
+                }
+
               }
             
           });
