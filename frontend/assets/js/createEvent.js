4a5812c9a47eb109b490aebb8d1a7682dd8f4da5
BillyGrind <denayeranthony623@gmail.com>
2023-05-26 14:25:09 +0200

    form ok

--- frontend/assets/js/createEvent.js
+++ frontend/assets/js/createEvent.js
@@ -1,70 +1,66 @@
+// import { format } from 'date-fns';
+
 export function createEvent(){
 
     //create Element for the form
-    let body =document.querySelector("body");
+    let body = document.querySelector("body");
     let newDiv = document.createElement("div");
     newDiv.classList.add("createEvent-container");
-    let buttonAdd = document.createElement("button");
-    buttonAdd.setAttribute("id","createEvent-button");
-    buttonAdd.textContent="Add an Event";
-    newDiv.appendChild(buttonAdd);
     body.appendChild(newDiv);
 
     // create content for the form
     let content = `
     <div class="createEvent-form">
-    <input type="text" id="createEvent-form-name" placeholder="name">
-    <input type="date" id="createEvent-form-date">
-    <input type="text" id="createEvent-form-author" placeholder="author">
-    <input type="text" id="createEvent-form-description"  placeholder="description">
+    <form id="createEvent-form">
+        <input type="text" id="createEvent-form-name" placeholder="name" required>
+        <input type="date" id="createEvent-form-date" required>
+        <input type="text" id="createEvent-form-author" placeholder="author" required>
+        <input type="text" id="createEvent-form-description"  placeholder="description" required>
+        <button type="submit" id="createEvent-button">Add Event</button>
+    </form>
     </div>
-     `;
-     newDiv.innerHTML += content;
+    `;
+    newDiv.innerHTML += content;
 
-     let buttonAddClick = document.querySelector("#createEvent-button");
+    let createEventForm = document.querySelector("#createEvent-form");
 
     // create function for POST date from form
-    buttonAddClick.addEventListener("click", () => {
+    createEventForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+
         let name = document.querySelector("#createEvent-form-name");
         let nameValue = name.value;
-        console.log(nameValue);
+        console.log("name => " + nameValue);
 
         let date = document.querySelector("#createEvent-form-date");
         let dateValue = date.value;
-        console.log(dateValue);
+        console.log("date => " + dateValue);
 
         let author = document.querySelector("#createEvent-form-author");
         let authorValue = author.value;
-        console.log(authorValue);
+        console.log("author => " + authorValue);
 
         let description = document.querySelector("#createEvent-form-description");
         let descriptionValue = description.value;
-        console.log(descriptionValue);
+        console.log("description => " + descriptionValue);
 
-
-        try{
+        try {
             fetch("http://localhost:3000/api/events/", {
-
-            method: "POST",
-        
-            body: JSON.stringify({ 
-              name: nameValue, 
-              dates:  [dateValue], 
-              author: authorValue, 
-              description: descriptionValue }),
-        
-            headers: {
-              "Content-type": "application/json; charset=UTF-8",
-            },
-          })
+                method: "POST",
+                body: JSON.stringify({ 
+                    name: nameValue, 
+                    dates:  [dateValue], 
+                    author: authorValue, 
+                    description: descriptionValue 
+                }),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8",
+                },
+            })
             .then((response) => response.json())
             .then((json) => console.log(json));
-
-        }catch(error){
-
+        } catch (error) {
             console.error("Error:", error);
-        
         }
-
-    })
+    });
 };
--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,26 +1,24 @@
 import { test } from "./test.js";
 import { createEvent } from "./createEvent.js";
 import { deleteEvent } from "./deleteEvent.js";
+import { displayEvent } from "./displayEvent.js";
 
 // test();
 
 createEvent();
-deleteEvent("54b304edfa2f");
+// deleteEvent();
+displayEvent();
+
 
 // src
 // https://www.geeksforgeeks.org/get-and-post-method-using-fetch-api/
 
 
-//Garder
-// createEvent.js; pour créer un event
-//
-
 
 //TO DO
-
- //  Allow users to add their availability to an existing event
+//  Allow users to add their availability to an existing event
 //  Edit an event name/description/author
-//  Delete an event
+
 //  You must validate your inputs before sending the data to your backend, inputs required must be filed and shorter than 256 characters. If it's not the case you don't send the request and display the appropriate error below the input.
 
 //DOING
@@ -29,3 +27,4 @@ deleteEvent("54b304edfa2f");
 
 //DO
 //  Allow users to create events 
+//  Delete an event
