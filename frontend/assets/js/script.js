ad7373aaa7b2e15e9e55b17b6220e9f44bef09fa
BillyGrind <denayeranthony623@gmail.com>
2023-05-26 10:56:42 +0200

    add function deleteEvent

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,7 +1,31 @@
 import { test } from "./test.js";
 import { createEvent } from "./createEvent.js";
+import { deleteEvent } from "./deleteEvent.js";
 
 // test();
+
 createEvent();
+deleteEvent("54b304edfa2f");
+
 // src
 // https://www.geeksforgeeks.org/get-and-post-method-using-fetch-api/
+
+
+//Garder
+// createEvent.js; pour créer un event
+//
+
+
+//TO DO
+
+ //  Allow users to add their availability to an existing event
+//  Edit an event name/description/author
+//  Delete an event
+//  You must validate your inputs before sending the data to your backend, inputs required must be filed and shorter than 256 characters. If it's not the case you don't send the request and display the appropriate error below the input.
+
+//DOING
+
+// Display all the events, including everyone's availability 
+
+//DO
+//  Allow users to create events 
